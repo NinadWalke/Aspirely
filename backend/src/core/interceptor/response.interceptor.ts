a57038bf1d98dff89b5390f8aee94df96d439454
Walky_117 <ninadwalke00@gmail.com>
2025-10-27 20:02:43 +0530

    feat: interceptor - response and cache
    Interceptors active. Response is now sent in a well structured manner with statusCode and timestamp. Caching is finally introducted with advanced modifications, where we don't store to cache unauth user data as well as we store only GET requests. Furthermore advancement to caching would be done when Redis is added to the stack, next hit is up for Exception Filters then Custom Decorators, after it we finally move to frontend

---
+++ backend/src/core/interceptor/response.interceptor.ts
@@ -0,0 +1,21 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+
+@Injectable()
+export class ResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    // pipe helps us change the structure or tap into a stream of data | map helps us modify, tap helps us observe
+    return next.handle().pipe(
+      map((data) => ({
+        success: true,
+        data,
+        timestamp: new Date().toISOString(),
+      })),
+    );
+  }
+}
